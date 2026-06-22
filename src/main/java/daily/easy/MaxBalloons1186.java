16250eed548e9e2a39d47a811364c679074e03be
rkm09 <raksha.mishra09@gmail.com>
2026-06-22 12:02:24 +0530

    max balloons

---
+++ src/main/java/daily/easy/MaxBalloons1186.java
@@ -0,0 +1,58 @@
+package daily.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxBalloons1186 {
+    public static void main(String[] args) {
+        String text = "loonbalxballpoon";
+        String text1 = "krhizmmgmcrecekgyljqkldocicziihtgpqwbticmvuyznragqoyrukzopfmjhjjxemsxmrsxuqmnkrzhgvtgdgtykhcglurvppvcwhrhrjoislonvvglhdciilduvuiebmffaagxerjeewmtcwmhmtwlxtvlbocczlrppmpjbpnifqtlninyzjtmazxdbzwxthpvrfulvrspycqcghuopjirzoeuqhetnbrcdakilzmklxwudxxhwilasbjjhhfgghogqoofsufysmcqeilaivtmfziumjloewbkjvaahsaaggteppqyuoylgpbdwqubaalfwcqrjeycjbbpifjbpigjdnnswocusuprydgrtxuaojeriigwumlovafxnpibjopjfqzrwemoinmptxddgcszmfprdrichjeqcvikynzigleaajcysusqasqadjemgnyvmzmbcfrttrzonwafrnedglhpudovigwvpimttiketopkvqw";
+        System.out.println(maxNumberOfBalloons(text1));
+    }
+
+    public static int maxNumberOfBalloons(String text) {
+        String target = "balloon";
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : text.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        int countB = 0, countA = 0, countL = 0, countO = 0, countN = 0;
+        for (var entry : map.entrySet()) {
+            char c = entry.getKey();
+            int val = entry.getValue();
+            String key = String.valueOf(c);
+            if (target.contains(key)) {
+                switch(c) {
+                    case 'l': countL = val/2; break;
+                    case 'o': countO = val/2; break;
+                    case 'b': countB = val; break;
+                    case 'a': countA = val; break;
+                    default: countN = val;
+                }
+            }
+        }
+
+        int count = Math.min(countA, Math.min(countB, countN));
+        count = Math.min(count, Math.min(countL, countO));
+
+        return count;
+    }
+}
+
+/*
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+Example 2:
+Input: text = "loonbalxballpoon"
+Output: 2
+Example 3:
+Input: text = "leetcode"
+Output: 0
+
+Constraints:
+1 <= text.length <= 10^4
+text consists of lower case English letters only.
+ */
