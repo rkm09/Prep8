9312dd5e76e918c819ea4208c5f31efe2f308d27
rkm09 <raksha.mishra09@gmail.com>
2026-07-21 18:18:52 +0530

    smallest sub

--- src/main/java/daily/medium/SmallestSubSeq1081.java
+++ src/main/java/daily/medium/SmallestSubSeq1081.java
@@ -12,9 +12,35 @@ public class SmallestSubSeq1081 {
     }
 
 
+//    greedy; time: O(n), space: O(1)
     public static String smallestSubsequence(String s) {
         int n = s.length();
-        return "";
+        int[] freq = new int[26];
+        boolean[] visited = new boolean[26];
+//        take the frequency
+        for (int i = 0; i < n; i++)
+            freq[s.charAt(i) - 'a']++;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+//            if not visited
+            if (!visited[ch - 'a']) {
+                while (!sb.isEmpty() && sb.charAt(sb.length() - 1) > ch) {
+                    if (freq[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        visited[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else
+                        break;
+                }
+                visited[ch - 'a'] = true;
+                sb.append(ch);
+            }
+//            decrement the count
+            freq[ch - 'a']--;
+        }
+
+        return sb.toString();
     }
 }
 
@@ -30,4 +56,10 @@ Output: "acdb"
 Constraints:
 1 <= s.length <= 1000
 s consists of lowercase English letters.
+ */
+
+/*
+First, consider a simpler problem: given a string s, how can we remove one character to make the resulting string
+lexicographically smallest?
+The answer is to find the smallest index i such that s[i]>s[i+1] and remove the character s[i].
  */
