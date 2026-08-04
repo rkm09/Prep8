4c19dd3bfe4c321ceac5f20b6a4be8083457db20
rkm09 <raksha.mishra09@gmail.com>
2026-08-04 08:44:56 +0530

    missing elem

---
+++ src/main/java/daily/easy/MissingElement3731.java
@@ -0,0 +1,60 @@
+package daily.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingElement3731 {
+    public static void main(String[] args) {
+        int[] nums = {1,4,2,5};
+        System.out.println(findMissingElements(nums));
+    }
+
+//    counting sort; time: O(n), space: O(1)
+    public static List<Integer> findMissingElements(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int num : nums) {
+           min = Math.min(min, num);
+           max = Math.max(max, num);
+        }
+        int len = max - min + 1;
+        int[] freq = new int[len];
+        for (int num : nums) {
+            freq[num - min]++;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (freq[i] == 0)
+                res.add(i + min);
+        }
+
+        return res;
+    }
+}
+
+/*
+You are given an integer array nums consisting of unique integers.
+Originally, nums contained every integer within a certain range. However, some integers might have gone missing from the array.
+The smallest and largest integers of the original range are still present in nums.
+Return a sorted list of all the missing integers in this range. If no integers are missing, return an empty list.
+Example 1:
+Input: nums = [1,4,2,5]
+Output: [3]
+Explanation:
+The smallest integer is 1 and the largest is 5, so the full range should be [1,2,3,4,5]. Among these, only 3 is missing.
+Example 2:
+Input: nums = [7,8,6,9]
+Output: []
+Explanation:
+The smallest integer is 6 and the largest is 9, so the full range is [6,7,8,9]. All integers are already present, so no integer is missing.
+Example 3:
+Input: nums = [5,1]
+Output: [2,3,4]
+Explanation:
+The smallest integer is 1 and the largest is 5, so the full range should be [1,2,3,4,5]. The missing integers are 2, 3, and 4.
+
+Constraints:
+2 <= nums.length <= 100
+1 <= nums[i] <= 100
+ */
